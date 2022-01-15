ae31803fc3744acf02f9f31c1db625c5b4902115
elbarrak mohamed <mohamedelbarrak@outlook.fr>
2022-01-15 23:21:45 +0100

    1

---
+++ jwt-youtube-master/src/main/java/com/youtube/jwt/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.youtube.jwt.repository;
+
+import com.youtube.jwt.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByProductName(String productName);
+
+    List<Product> findAllByCategoryAndPromotion(Category category , Promotion promotion);
+
+    Optional<Product> findByProductIdAndCategoryCategoryIdAndPromotionPromotionId(Long id , Long categoryId , Long promotionId);
+
+
+}
